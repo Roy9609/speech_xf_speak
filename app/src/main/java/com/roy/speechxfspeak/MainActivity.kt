8b21b748c98d82eb719f25cb9e925f3d6fe402ba
Roy <leiyun.chen@clife.cn>
2025-07-12 10:10:01 +0800

    1.aar

---
+++ app/src/main/java/com/roy/speechxfspeak/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.roy.speechxfspeak
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.iflytek.sparkchain.core.tts.PersonateTTS
+
+/**
+ *    desc   :
+ *    date   : 2025/7/12 10:01
+ *    author : Roy
+ *    version: 1.0
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+      var a =  PersonateTTS("x5_lingyuyan_flow")
+    }
+}
